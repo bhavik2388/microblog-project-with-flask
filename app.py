261de49e5b30a7450bc4bf695b18840ed2a6b513
Bhavik Paun <bhavikpaun18@gmail.com>
2021-11-29 14:59:09 +0530

    v5

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/") 
+class GalileanMoons:
+    def __init__ (self,first,second,third,fourth):
+        self.first = first
+        self.second = second
+        self.third = third
+        self.fourth = fourth
+
+@app.route("/datast") 
 def hello_world():
-    color = "brown"
-    animal_one = "fox"
-    animal_two = "dog"
-
-    orange_amount = 10
-    apple_amount = 20
-    donate_amount = 15
-
-    first_name = "Captain"
-    last_name = "Marvel"
-
-    keys =  {
-            "color" : color,
-            "animal_one" : animal_one,
-            "animal_two" : animal_two,
-            "orange_amount" : orange_amount,
-            "apple_amount" : apple_amount,
-            "donate_amount" : donate_amount,
-            "first_name" : first_name,
-            "last_name" : last_name
+    movies = [
+        "Avenger",
+        "Spiderman",
+        "Ironman"
+    ]
+    car = {
+        "brand":"tesla",
+        "model":"roadster",
+        "year": "2020"
     }
 
-    return render_template("expressions.html",**keys)
+    moons = GalileanMoons("Io","Europa","Ganymede","Callisto")
+
+    return render_template("data_structures.html",movies=movies,car=car,moons=moons)
